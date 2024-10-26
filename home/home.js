1a708b64175848f85c5439a6aaa03436d0f60210
Rohit Kumar Mahato <rohitkumarmahato2511@gmail.com>
2024-10-26 19:15:44 +0530

    invite button funciton added

--- home/home.js
+++ home/home.js
@@ -99,3 +99,6 @@ document.addEventListener('DOMContentLoaded', function () {
     showSlide(currentSlide);
   });
 
+  function openWhatsApp() {
+    window.open("https://wa.me/?text=Join%20us%20in%20making%20a%20difference!%20Share%20the%20love%20and%20inspire%20change%20by%20inviting%20friends%20to%20Helping%20Hut.%20Together,%20we%20uplift%20lives%20and%20create%20a%20brighter%20future.%20%20You%20can%20also%20volunteer%20with%20us%20and%20be%20part%20of%20something%20amazing!%20%20Let's%20make%20a%20positive%20impact,%20one%20step%20at%20a%20time.");
+  }
